39ad550338e6b326db41fff92fd91d8263b9cd57
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-02-10 13:58:17 +0800

    added schematell API and schematell

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const links = [
-    //{label: "Webpage", url: "http://127.0.0.1:4040"},
-    {label: "Schematell API", url: "http://api.schematell.up.railway.app"},
-    {label: "Schematell", url: "http://schematell.up.railway.app"},
+    {label: "Webpage", url: "http://127.0.0.1:4040"},
+    {label: "Schematell API", url: "https://api.schematell.up.railway.app"},
+    {label: "Schematell", url: "https://schematell.up.railway.app"},
 ]
 
 function div(name, status, quote, isError){
@@ -36,7 +36,7 @@ async function main() {
             }).then((res) => res.json());
             const status = result.status;
             let isError = true
-            if (toString(status).startsWith("2")){
+            if (status.toString().startsWith("2")){
                 isError = false
             }
             const container = div(links[i].label, status, getStatus(status), isError);
